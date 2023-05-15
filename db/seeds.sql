612ded06f7cfd1a1895983eaf50b7f37e1ba8dcc
Ms Eko <jomaduagwu@gmail.com>
2023-05-15 18:32:44 -0500

    updated dbs

--- db/seeds.sql
+++ db/seeds.sql
@@ -5,20 +5,35 @@ VALUES ("HR"),
        ("Sales & Marketing"),
        ("Finance");
 
-INSERT INTO roles (title, salary, department_id)
-VALUES ("Daa Analyst", '85000', 2),
+INSERT INTO role (title, salary, department_id)
+VALUES ("Data Engineer", '95000', 2),
+       ("Data Analyst", '85000', 2),
+       ("Sales Manager", '80000', 4),
        ("Salesman", '60000', 4),
-       ("Admin Assistant", '60000' 3),
+       ("Admin Manager", '82000', 3),
+       ("Admin Assistant", '60000', 3),
        ("Manager", '105000', 5),
-       ("Data Engineer", '95000', 2),
        ("HR Manager", '75000', 1),
        ("HR Generalist", '60000', 1);
        
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Adam", "Johnson", 1, 5)
+VALUES ("Adam", "Johnson", 1, null),
        ("Scarlet", "Miller", 2, null),
        ("Ivy", "Park", 3, null),
-       ("Jon", "Brown", 4, null),
+       ("Jon", "Brown", 4, 3),
        ("Teresa", "Alvarez", 5, null ),
-       ("Dwayne", "Smith", 6, null ),
-       ("Anita", "Davis", 7, 6);
+       ("Dwayne", "Smith", 6, 5 ),
+       ("Anita", "Davis", 7, null),
+       ("Hilda", "Moore", 8, null),
+       ("Brierly", "White", 9, null);
+
+-- INSERT INTO employee (first_name, last_name, role_id, manager_id)
+-- VALUES ("Adam", "Johnson", 1, 5),
+--        ("Scarlet", "Miller", 2, 8),
+--        ("Ivy", "Park", 3, 9),
+--        ("Jon", "Brown", 4, null),
+--        ("Teresa", "Alvarez", 5, null ),
+--        ("Dwayne", "Smith", 6, null ),
+--        ("Anita", "Davis", 7, 6),
+--        ("Hilda", "Moore", 8, null),
+--        ("Brierly", "White", 9, null);
